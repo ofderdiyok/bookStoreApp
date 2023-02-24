a231b5ef5bdf556dd0488567b75f64f00d0fd481
Faruk Derdiyok <farukderdiyok01@gmail.com>
2023-02-24 12:40:26 +0300

    Category component edited. Favorite list added.

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -18,4 +18,17 @@ export class BooksComponent {
     this.bookRepository = new BookRepository();
     this.books = this.bookRepository.getBooks();
   }
+
+  addToList($event: any, book: Book){
+    
+    if($event.target.classList.contains('fa-heart')){
+      $event.target.classList.remove('fa-heart');
+      $event.target.classList.add('fa-trash');
+      $event.target.innerText = " Remove"
+    }else {
+      $event.target.classList.add('fa-heart');
+      $event.target.classList.remove('fa-trash');
+      $event.target.innerText = " Add"
+    }
+  }
 }
